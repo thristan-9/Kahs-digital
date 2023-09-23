bb27382f87ff3a7a3fed335247560c9d90faac91
thristan gabriel pereira deretti <thristanxyz@gmail.com>
2023-09-23 18:31:28 -0300

    save

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Hero from './components/pages/hero';
+import About from './components/pages/about';
+import NavBar from './components/navbar';
+
+
+const App = () => (
+  <div>
+    <NavBar />
+    <Hero/>
+    <About />
+  </div>
+)
+
+
+export default App
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './navbar.module.css'
+
+function NavBar() {
+  return (
+    <div className={style.section}>NavBar</div>
+  )
+}
+
+export default NavBar
