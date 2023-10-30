c848c3581bdd7222c65c6a7093ff829011c4bccc
thristan gabriel pereira deretti <thristanxyz@gmail.com>
2023-10-30 10:19:02 -0300

    save

--- src/components/pages/Faqs.tsx
+++ src/components/pages/Faqs.tsx
@@ -1,32 +1,55 @@
+import { useRef } from "react";
 import style from "../../styles/Faqs.module.css";
 import { faqs } from "../../constants";
 
 export default function Faqs() {
+
+    const refSlide = useRef([]);
+
+    function moveLeft() {
+        if (refSlide?.current) {
+            console.log("refSlide.current.index", refSlide.current.key)
+        }
+    }
+
+    function moveRight() {
+        if (refSlide?.current) {
+            const index = refSlide.current;
+            console.log("index", index)
+        }
+    }
+
     return (
         <section id="faqs" className={style.faqs}>
-            <div className={`${style.faqs__container}  grid`}>
+            <div className={`${style.faqs__container} container`}>
+                
                 <div className={`${style.faqs__data}`}>
 
-                    <div className={style.slider}>
-                        <div className={style.icon__wrapper}>
-                            <span className={`material-icons-outlined icon ` + style.faqs__icon}>arrow_back_ios</span>
-                        </div>
-                        {faqs.map((item, index) => {
-                            return (
-                                <div id={"faqs-slide-" + (index + 1)} key={index} className={style.slide}>
-                                    <h1 className={style.slide__title}>
-                                        {item.question}
-                                    </h1>
+                    <div onClick={() => moveLeft()} className={`${style.icon__wrapper} ${style.icon__left}`}>
+                        <span className={`material-icons-outlined icon ` + style.faqs__icon}>arrow_back_ios</span>
+                    </div>
 
-                                    <h3 className={style.slide__description}>
-                                        {item.answer}
-                                    </h3>
-                                </div>
-                            )
-                        })}
-                        {/* <div className={style.icon__wrapper}>
-                            <span className="material-icons-outlined icon">arrow_forward_ios</span>
-                        </div> */}
+                    <div onClick={()=> moveRight()} className={`${style.icon__wrapper} ${style.icon__right}`}>
+                        <span className={`material-icons-outlined icon ` + style.faqs__icon}>arrow_forward_ios</span>
+                    </div>
+
+                    <div className={style.slider}>
+                        <ul>
+                            {faqs.map((item, index) => {
+                                return (
+                                    <li ref={el => refSlide.current[index] = el} id={"faqs-slide-" + (index + 1)} key={index}>
+                                        <h1 className={style.slide__title}>
+                                            {item.question}
+                                        </h1>
+                                        <div className={style.slide__description_center}>
+                                            <h3 className={style.slide__description}>
+                                                {item.answer}
+                                            </h3>
+                                        </div>
+                                    </li>
+                                )
+                            })}
+                        </ul>
                     </div>
                     
                     <div className={style.dots}>
